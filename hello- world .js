947ae2516df966c80c28eb8262088e483f0ce963
Ericadp.developer <ericadp.developer@gmail.com>
2020-05-30 19:03:14 -0400

    Add files via upload

---
+++ hello- world .js
@@ -0,0 +1,12 @@
+/* 
+
+Title: Perry-assignment-1.4.js
+Author: Erica Perry
+Date:5/30/2020
+Description: Hello- World
+*/
+
+console.log("Hello World, my name is Erica Perry");
+
+// Variable to hold a messasge
+var message;
