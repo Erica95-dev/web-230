0ad8db8a8f92fd3c276ecb1c9e39fee6173371bc
Ericadp.developer <ericadp.developer@gmail.com>
2020-05-30 22:48:31 -0400

    Add files via upload

---
+++ perry-assignment -1.5.js
@@ -0,0 +1,74 @@
+/* 
+
+Title: Perry-assignment-1.5.js
+Author: Erica Perry
+Date:5/30/2020
+Description: types,values and variables
+*/
+
+// start program: Github repro
+
+/*
+
+Expected output:
+
+3 firstName
+3 lastName
+3 address
+3 payRate
+3 hireDate
+*/
+
+ var firstName = "Quanny";
+ var lastName = "Whiltshire";
+ // variable address where employee currently lives
+ var address = "62 Gotluck St. Lynn MA 01234";
+ //variable of employee's current payrate to exponential 2234.0
+ var  payRate = "2.2e+3";
+ //employee's hire date
+ var hireDate = "(2018/04/05)";
+
+ //output
+ console.log(firstName);
+ console.log(lastName);
+ console.log(address);
+ console.log(payRate);
+ console.log(hireDate);
+
+
+ var firstName = "Britto";
+ var lastName = "Johnson";
+ // variable address where employee currently lives
+ var address = "123 Happy St. Boston MA 01254";
+ //variable of employee's current payrate to exponential 22340.0
+ var payRate= "2.2e+4";
+ // employee's hire date
+ var hireDate = "(2015/06/03)";
+
+ //output
+ console.log(firstName);
+ console.log(lastName);
+ console.log(address);
+ console.log(payRate);
+ console.log(hireDate);
+
+
+
+ var firstName = "Richard";
+ var lastName = "Davidson";
+ // variable address where employee currently lives
+ var address = "18 Poplar St. Revere MA 01284";
+ //variable of employee's current payrate to exponential 223409.0
+ var payRate= "2.2e+5";
+ //employee's hire date
+ var hireDate = "(2018/09/05)";
+
+ // output
+ console.log(firstName);
+ console.log(lastName);
+ console.log(address);
+ console.log(payRate)
+ console.log(hireDate)
+
+
+ //end program›
