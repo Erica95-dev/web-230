4d7a504ef3ddcaec0ea31cc97776b09a40ce85b0
Ericadp.developer <ericadp.developer@gmail.com>
2020-05-30 23:47:37 -0400

    Add files via upload

---
+++ perry-assignment-1.4.js
@@ -0,0 +1,39 @@
+/* 
+
+Title: Perry-assignment-1.4.js
+Author: Erica Perry
+Date:5/30/2020
+Description: primitive types
+*/
+
+// start program: Github repro
+
+/*
+
+Expected output:
+
+firstName
+lastName
+age
+city
+zipCode
+*/
+
+var firstName = "Erica";
+var lastName ="Perry";
+var age = "25";
+var street = "3 Surry Lane";
+// an address for Erica
+var city ="Lynn";
+// A city in Boston Ma
+var zipCode = "01981";
+
+//output
+console.log (firstName);
+console.log (lastName);
+console.log (age);
+console.log (street);
+console.log (city);
+console.log (zipCode);
+
+//end program
