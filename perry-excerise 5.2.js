53575d6954d9e1eac446f2c4f2232ee6e1d2c298
Ericadp.developer <ericadp.developer@gmail.com>
2020-06-26 11:08:05 -0400

    Create perry-excerise 5.2.js

---
+++ perry-excerise 5.2.js
@@ -0,0 +1,25 @@
+/* 
+Title: perry-excerise 5.2.js
+Author: Erica Perry
+Date:6/26/2020
+Modified: Erica Perry
+Description: ES5 Built-in Functions:
+*/
+
+/*
+  Expected output:
+  FirstName LastName
+  Exercise 5.2
+  Today's Date
+  Oysters
+  Shrimp
+  Steak
+  Tacos
+  Sushi
+*/
+
+
+var foods = ["Oysters", "Shrimp", "Steak", "Tacos", "Sushi"]; // one dimesion array
+
+ foods.forEach(function(food) // calling the functions foods,forEach and creating an anonymous function
+{console.log(food)});
